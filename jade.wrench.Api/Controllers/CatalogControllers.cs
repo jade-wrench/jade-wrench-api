9ccac4d646b144649ea13e7192452c8f18ff16b7
mattcopfer <21copfma@gmail.com>
2025-03-25 18:02:15 -0400

    Added get item by ID

--- jade.wrench.Api/Controllers/CatalogControllers.cs
+++ jade.wrench.Api/Controllers/CatalogControllers.cs
@@ -17,5 +17,12 @@ namespace jade.wrench.Api.Controllers
         };
             return Ok(items);
         }
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id){
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
+        }
     }
 }
