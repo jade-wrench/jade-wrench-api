19639673bc8656318cda6155ea995ab49af921b6
mattcopfer <21copfma@gmail.com>
2025-03-25 20:23:51 -0400

    Added post, put, and delete methods

--- jade.wrench.Api/Controllers/CatalogControllers.cs
+++ jade.wrench.Api/Controllers/CatalogControllers.cs
@@ -28,5 +28,20 @@ namespace jade.wrench.Api.Controllers
         public IActionResult Post(Item item){
             return Created("/catalog/42",item);
         }
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating){
+            var item = new Item("Shirt", "Ohio State shirt", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+            return Ok(item);
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item){
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id){
+            return NoContent();
+        }
     }
 }
